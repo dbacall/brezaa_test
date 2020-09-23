b6a3b372a1449e07050686f468319a235bfaea9f
dbacall <dbacall@hotmail.co.uk>
2020-09-23 10:53:30 +0100

    chore: connect to mongodb database

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://dbacall:brezaa@cluster0.fezcl.mongodb.net/brezaa?retryWrites=true&w=majority',
+  secretOrKey: process.env.SECRET || 'secret',
+};
