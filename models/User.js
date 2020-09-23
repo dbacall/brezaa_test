861cca386734592331d84f5653db61748c2299f9
dbacall <dbacall@hotmail.co.uk>
2020-09-23 11:11:47 +0100

    feat: add user model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  typeOfUser: {
+    type: String,
+    required: true,
+  },
+  profession: {
+    type: String,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
